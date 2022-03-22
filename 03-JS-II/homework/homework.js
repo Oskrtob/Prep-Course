29fb3295f4caae527bf01cfea000a6090c377b41
Oskrtob <oskr-tob@hotmail.com>
2022-03-22 18:05:25 -0500

    homeworkJSIII

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -179,9 +179,9 @@ function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-  if (valor == true){
+  if (valor === true){
     return "Soy verdadero";
-  } else if (valor == false){
+  } else if (valor === false){
     return "Soy falso";
   }
 }
